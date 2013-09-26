de94c66704f9010d8e76075cb5959a64efe9dde7
blueboar2 <blueboar2@gmail.com>
2013-09-26 23:05:26 +0600

    main

--- main.c
+++ main.c
@@ -1,3 +1,14 @@
+#include <stdio.h>
+
+#include <gmp.h>
+#include <mpfr.h>
+
 int main () {
+    mpfr_t s;
+
+    mpfr_init2 (s, 200);
+    mpfr_set_d (s, 1.0, MPFR_RNDD);
+    mpfr_out_str (stdout, 10, 0, s, MPFR_RNDD);
+    mpfr_clear (s);
     return 0;
     }

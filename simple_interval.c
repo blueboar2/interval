f917d568083a1654cab017e4045fbda78aa7e121
blueboar2 <blueboar2@gmail.com>
2013-10-08 23:22:26 +0600

    1

---
+++ simple_interval.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <glib.h>
+
+#include "simple_interval.h"
+
+GList * simple_interval (char * neravenstvo)
+    {
+    GList *answer = NULL;
+
+    printf ("%s %s", "Very simple: ", neravenstvo);
+    return 0;
+    }

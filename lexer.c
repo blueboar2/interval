b5e050049bf41a3afac3a0647644c622d3c0e114
blueboar2 <blueboar2@gmail.com>
2013-10-08 23:22:49 +0600

    1

--- lexer.c
+++ lexer.c
@@ -3,5 +3,40 @@
 %}
 
 %%
-"+"                  { return PLUS; }
+[-+]?[0-9]*[\.\,]?[0-9]+([eE][-+]?[0-9]+)?		{ return REALNUMBER; }
+"+inf"							{ return PLUSINF; }
+"-inf"							{ return MINUSINF; }
+"U"							{ return UNIF; }
+
+"<="							{ return LESSEQUAL; }
+">="							{ return GREATEREQUAL; }
+"<"							{ return LESS; }
+">"							{ return GREATER; }
+
+"exp"							{ return EXP; }
+"ln"							{ return LN; }
+"sin"							{ return SIN; }
+"cos"							{ return COS; }
+"tan"							{ return TAN; }
+"tg"							{ return TAN; }
+"cotan"							{ return COTAN; }
+"ctan"							{ return COTAN; }
+"ctg"							{ return COTAN; }
+
+"**"							{ return POWER; }
+
+\+							{ return PLUS; }
+\-							{ return MINUS; }
+\*							{ return MULTIPLY; }
+\\							{ return DIVIDE; }
+"("							{ return OKRSKOB; }
+")"							{ return ZKRSKOB; }
+"["							{ return OKVSKOB; }
+"]"							{ return ZKVSKOB; }
+";"							{ return TOZA; }
+"^"							{ return POWER; }
+
+"x"							{ return IKS; }
+
+.							{ return 0; }
 %%
